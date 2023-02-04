e4d38e92b8d59c0f387f4986755fbfba92addee1
ETiurin <117716801+ETiurin@users.noreply.github.com>
2023-02-04 12:06:28 +0300

    edited index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,64 +1,37 @@
-const editButton = document.querySelector(".profile__edit-button");
-const popup = document.querySelector(".popup");
-const closeButton = popup.querySelector(".popup__close");
 
-const toggleOpenPopup = () => {
-  popup.classList.toggle("popup_opened");
-};
+const openEditPopup = document.querySelector(".profile__edit-button");
+const popupContainer = document.querySelector(".popup");
+const popupBtnClose = document.querySelector(".popup__close");
+const formDescription = document.querySelector(".popup__form");
 
-const handleeditButtonClick = () => {
-  toggleOpenPopup();
-};
+let profileName = document.querySelector(".profile__name");
+let profileAbout = document.querySelector(".profile__about");
+let userNameInput = document.querySelector(".popup__input_type_name");
+let profileAboutInput = document.querySelector(".popup__input_type_about");
 
-const handleCloseButtonClick = () => {
-  toggleOpenPopup();
-};
-const handleOverlyClick = (event) => {
-  if (event.target === event.currentTarget) {
-    toggleOpenPopup();
-  }
-};
-editButton.addEventListener("click", handleeditButtonClick);
-closeButton.addEventListener("click", handleCloseButtonClick);
-popup.addEventListener("click", handleOverlyClick);
-
-let formElement = document.querySelector(".popup__container");
-let nameInput = document.querySelector(".popup__input_type_name");
-let aboutInput = document.querySelector(".popup__input_type_about");
-console.log(nameInput);
-console.log(aboutInput);
-
-
-const saveButton = popup.querySelector(".popup__button");
-const handleSaveButtonClick = () => {
-  popup.classList.remove("popup_opened");
+function openPopup() {
+  userNameInput.value = profileName.textContent;
+  profileAboutInput.value = profileAbout.textContent;
+  popupContainer.classList.add("popup_opened")
 }
-saveButton.addEventListener('click', handleSaveButtonClick);
 
+function closePopup() {
 
-// Обработчик «отправки» формы
-function handleFormSubmit (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-          // Так мы можем определить свою логику отправки.
-         // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
-    let formNameElement = popup.querySelector(".popup__input_type_name");
-    let formAboutElement = popup.querySelector(".popup__input_type_about");
-    
-    console.log(formNameElement.value);
-    console.log(formAboutElement.value);
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
-
-    function nameInputTextContent (){
-
-      nameInput.textContent = 'Имя';
-      aboutInput.textContent = 'О себе';
-    }
-    nameInput.addEventListener("click", handleCloseButtonClick);
+  popupContainer.classList.remove("popup_opened")
 }
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', handleFormSubmit);
+
+// Функция редактирования окна
+
+function editinProfileName(evt) {
+
+  evt.preventDefault();
+
+  profileName.textContent = userNameInput.value;
+  profileAbout.textContent = profileAboutInput.value;
+
+  closePopup();
+}
+
+openEditPopup.addEventListener("click", openPopup)
+popupBtnClose.addEventListener("click", closePopup)
+formDescription.addEventListener("button", editinProfileName);
